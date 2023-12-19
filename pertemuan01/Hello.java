edfef59b926be4e26f8bb1d3398036423d1b02d8
dzzhar <zharifahdzikra@gmail.com>
2023-12-19 16:16:34 +0700

    intro PDPL dan PBO

---
+++ pertemuan01/Hello.java
@@ -0,0 +1,15 @@
+package pertemuan01;
+
+// setiap program paling tidak memiliki satu class 
+// pilih nama class dan deskripsikan aksi dari program
+// nama class harus diawali dengan huruf kapital
+public class Hello {
+
+    // program java mengandung metode main di header ini
+    public static void main(String[] args) {
+
+        // statement didalam main method bakal dijalanin ketika program dijalankan
+        // tutup dengan semicolon
+        System.out.println("halo vindi, selamat belajar JAVA");
+    }
+}
---
+++ pertemuan01/Intro.java
@@ -0,0 +1,20 @@
+package pertemuan01;
+
+public class Intro {
+    public static void main(String[] args) {
+        String namaLengkap = "Zharifah Dzikra Purnomo";
+        int umur = 19;
+        String tempatLahir = "Jakarta";
+        String tanggalLahir = "16 Oktober 2003";
+        String namaKampus = "STT Terpadu Nurul Fikri";
+        String programStudi = "Teknik Informatika";
+        String nim = "0110222050";
+
+        System.out.println("Nama Lengkap : " + namaLengkap);
+        System.out.println("Umur : " + umur);
+        System.out.println("Tempat, tanggal lahir: " + tempatLahir + ", " + tanggalLahir);
+        System.out.println("Kampus : " + namaKampus);
+        System.out.println("Program Studi : " + programStudi);
+        System.out.println("NIM : " + nim);
+    }
+}
