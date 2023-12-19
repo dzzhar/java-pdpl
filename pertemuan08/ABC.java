16f023c029971ebe45cd0cf90f94ac9a1b5659c6
dzzhar <zharifahdzikra@gmail.com>
2023-12-19 16:29:24 +0700

    creational pattern: prototype

---
+++ pertemuan08/ABC.java
@@ -0,0 +1,4 @@
+public class ABC {
+    // instance variable of the class ABC
+    int x = 9;
+}
---
+++ pertemuan08/ShallowCopyExample.java
@@ -0,0 +1,17 @@
+public class ShallowCopyExample {
+    // main method
+    public static void main(String argvs[]) {
+        // creating an object of the class ABC
+        ABC obj1 = new ABC();
+
+        // it will copy the reference, not value
+        ABC obj2 = obj1;
+
+        // updating the value to 45
+        // using the reference variable obj2
+        obj2.x = 45;
+
+        // printing the value of x using reference variable obj1
+        System.out.println("The value of x is: " + obj1.x);
+    }
+}
